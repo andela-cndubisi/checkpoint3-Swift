352f3e71bb47eb0570a6b3457b98ffc6757f604d
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-11-09 14:39:43 +0100

    refactor model to calculate recursively using enums

--- CurrencyCalculator/Calculator.swift
+++ CurrencyCalculator/Calculator.swift
@@ -2,72 +2,82 @@
 //  Calculator.swift
 //  CurrencyCalculator
 //
-//  Created by andela-cj on 03/11/2015.
+//  Created by Chijioke Ndubisi on 05/11/2015.
 //  Copyright © 2015 andela-cj. All rights reserved.
 //
 
-import Foundation
-
-class Calculator {
-    private var memory = Array<Double>()
-    var opera = operation()
-    var isTyping = false
-    var period = false
-    var temp:String?
-    
-    func addDigigt(digit:String){
-        let dot = "."
-        if (isTyping){
-            temp! += digit
-            if (digit == dot){
-                period = true
-            }
-        }else{
-            temp! = digit
-            if (digit == dot){
-                period = true
-                temp! = "0"+digit
-            }
-            isTyping = true
-        }
-    }
-    
-    func updateOperation(operation: String){
-        if (operation != opera.currentOperation){
-            opera.currentOperation = operation
-            evaluate()
-            isTyping = false
-        }
-    }
-
-    func clear(){
-        temp = "0"
-        isTyping = false
-    }
-    func evaluate(){
-        if(memory.count >= 2){
-            if (opera.currentOperation != nil) {
-                memory.append(opera.evaluate(self.memory.removeLast(),op2:self.memory.removeLast())!)
-                opera.currentOperation = nil
-            }
-        }else{
-            memory.append(Double(temp!)!)
-        }
-    }
-    
-    struct operation {
-        var currentOperation:String?
-        func evaluate(op1:Double, op2:Double) -> Double?{
-            if (currentOperation != nil){
-                switch currentOperation!{
-                case "×": return op1 * op2
-                case "+": return op1 + op2
-                case "÷": return op1 / op2
-                case "−": return op1 - op2
-                default: return nil
+struct Brain {
+    var opStack = [Op]()
+    var isTyping:Bool = false
+    var knownOps = [String:Op]()
+    var currentOperation:Op?{
+        willSet{
+            if (currentOperation != nil) {
+                if opStack.count > 2{
+                    result = evaluate()
                 }
             }
-            return nil
+        }
+        didSet{
+            opStack.append(currentOperation!)
+        }
+    }
+    var result:Double?{
+        didSet{
+            opStack.removeAll()
+            opStack.append(.operand(result!))
+            
+        }
+    }
+
+    
+    mutating func addDigit(digit:Double){
+        opStack.append(.operand(digit))
+        isTyping = false
+    }
+    
+    init(){
+        knownOps["×"] = Op.binaryOperation("×",*)
+        knownOps["+"] = Op.binaryOperation("+",+)
+        knownOps["÷"] = Op.binaryOperation("÷",{$1/$0})
+        knownOps["−"] = Op.binaryOperation("−",{$1-$0})
+    }
+
+    func evaluate (ops:[Op], _ firstOperand:Double?)-> (result: Double?, remainingOps:[Op]){
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op{
+            case .operand(let operand):
+               return remainingOps.isEmpty ?   (operand, remainingOps): evaluate(remainingOps,operand)
+            case .binaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps,firstOperand)
+                if let operand2 = op1Evaluation.result {
+                    return (operation(firstOperand!,operand2), op1Evaluation.remainingOps)
+                }
+            }
+        }
+        return (nil, ops)
+    }
+    
+    func evaluate()->Double?{
+        let (result, _) = evaluate(opStack, nil)
+        return result
+    }
+
+    enum Op{
+        case binaryOperation(String, (Double, Double)-> Double)
+        case operand(Double)
+    }
+    
+    mutating func pushOperand(digit:Double){
+        opStack.append(.operand(digit))
+    }
+    
+    mutating func performOperation(symbol:String){
+        if let operation = knownOps[symbol]{
+            currentOperation = operation
+            isTyping = false
         }
     }
 }
